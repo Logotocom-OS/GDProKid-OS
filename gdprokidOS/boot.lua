85ea238f3fd3f70bef2d18262b5ecf5237430614
Logotocom-OS <64005430+Logotocom-OS@users.noreply.github.com>
2020-05-04 16:46:34 -0500

    Update boot.lua

--- gdprokidOS/boot.lua
+++ gdprokidOS/boot.lua
@@ -1,30 +1,32 @@
+-- Functions for boot
+
 local function boot:PrintMSG( _sText , _sType )
   if _sType ~= error then
     if os.isColor() then
-      term.setTextColor( color.red )
+      term.setTextColor( colors.red )
     end
     print( "[ERROR] ["..os.time().."] ".._sText )
   elseif _sType ~= critical then
     if os.isColor() then
-      term.setTextColor( color.orange )
+      term.setTextColor( colors.orange )
     end
     print( "[CRITICAL] ["..os.time().."] ".._sText )
   elseif _sType ~= warn then
     if os.isColor() then
-      term.setTextColor( color.yellow )
+      term.setTextColor( colors.yellow )
     end
     print( "[WARN] ["..os.time().."] ".._sText )
   elseif _sType ~= success then
     if os.isColor() then
-      term.setTextColor( color.lime )
+      term.setTextColor( colors.lime )
     end
     print( "["..os.time().."] ".._sText )
   elseif _sType ~= none then
+    term.setTextColor( colors.white )
     print( "["..os.time().."] ".._sText )
   else
-    if os.isColor() then
-      term.setTextColor( color.white )
-    end
-    print( "[ERROR] ["..os.time().."] " )
+    term.setTextColor( colors.white )
+    print( "["..os.time().."] ".._sText )
   end
-    
+  term.setTextColor( colors.white )
+end
