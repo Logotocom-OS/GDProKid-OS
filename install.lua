e3e20595c80b7a9d2e0b04712a6b01e91230a140
Logotocom-OS <64005430+Logotocom-OS@users.noreply.github.com>
2020-05-04 23:13:59 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,6 +1,18 @@
 -- -- -- Installation of GDProKid-OS v1.x -- -- --
 -- Functions
-
+local function getAndSave(url, filename)
+	local src = http.get(url)
+	if src == nil then
+		print("Failed to get file: "..url)
+		return false
+	else
+		local file = fs.open(filename,"w")
+		file.write(src.readAll())
+		file.close()
+		src.close()
+	end
+	return true
+end
 
 -- Main
 term.clear()
