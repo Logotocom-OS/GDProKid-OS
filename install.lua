4e0df9a522279ea29f3e107a2b885df36fa99241
Logotocom-OS <64005430+Logotocom-OS@users.noreply.github.com>
2020-05-04 21:43:40 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,2 +1,9 @@
--- Installation of GDProKid-OS v1.x
+-- -- -- Installation of GDProKid-OS v1.x -- -- --
+-- Functions
 
+
+-- Main
+term.clear()
+term.setCursorPos(1,1)
+write( "INSTALL GUIDE" )
+term.setCursorPos(1.3)
