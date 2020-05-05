f19dc4fc3d591c952593918e7982ba40a265002c
Logotocom-OS <64005430+Logotocom-OS@users.noreply.github.com>
2020-05-05 01:18:48 -0500

    Update important_stuff.lua

--- important_stuff.lua
+++ important_stuff.lua
@@ -1,6 +1,17 @@
--- Importat Functions and stuff.
+-- Importat Functions and stlocaluff.
+local function 
+
 local function os.log( _sText, _sType )
   local file = fs.open("/gdprokidOS/logs/system.log","w")
   file.writeLine("[SYS - " .. os.time() .. "] " .. _sText )
   file:close()
 end
+
+local function sys.getTime( _sType )
+  -- Enter "os.time()" so it can caculate the date.
+  -- Or make your own program by using this function to make awesome time caculators!
+  -- Remember to show me on pastebin by emailing me at bhowell0009@gmail.com
+  -- I will add it in the next update!
+  -- PS: It uses the GUI api.
+  
+  
